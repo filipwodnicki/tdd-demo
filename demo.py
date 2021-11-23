4096f02685a6e050e240e3f565a2fe078996705d
Filip Wodnicki <filip.wodnicki@gmail.com>
2021-11-23 18:30:30 +0100

    Set up initial process_image function
    This is the first step. We set up the skeleton of the project code, thinking of the functionality we want to build.
    The next step will be to write comprehensive tests for the function.

---
+++ demo.py
@@ -0,0 +1,20 @@
+"""demo.py
+Small demo file that shows corresponding tests.
+"""
+import numpy as np
+
+
+def process_image(image_array: np.array) -> np.array:
+    """Converts image to grayscale and returns normalized 0 to 1
+
+    # Input
+    image_array (np.array) -> image that is 10 x 10 x 3 in 8-bit (0-255)
+
+    # Output
+    image_array (np.array) -> image that is 10 x 10 x 1 grayscale, normalized (0-1)
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
