1b303da541f6e0209e16f1bb523ba2eeb28c7261
Filip Wodnicki <filip.wodnicki@gmail.com>
2021-11-23 19:02:25 +0100

    Implement _convert_to_grayscale helper function along with tests
    We implement the code to convert to grayscale and discover a few things along the way.
    First, with three "bands", the number of bands is actually the first dimension of the numpy array.
    The function code has been written reflect this, and tests were tweaked as well.
    
    Second, numpy specifies a data type of 8-bit integer, which should be reflected in both the code and the tests.
    Now, we're only done testing the numpy shapes. Let's write the test to check how grayscale averaging actually functions.

--- demo.py
+++ demo.py
@@ -4,6 +4,21 @@ Small demo file that shows corresponding tests.
 import numpy as np
 
 
+def _convert_to_grayscale(image: np.array) -> np.array:
+    """Converts 8-bit image array from RGB to grayscale by averaging RGB inputs"""
+    assert image.dtype == np.dtype("uint8"), "array not 8-bit unsigned"
+    assert len(image.shape) == 3, "image array needs to have three dimensions"
+    assert image.shape[0] == 3, "image needs to be composed of three layers, RGB"
+
+    return np.mean(image, axis=0)
+
+
+def _normalize(image: np.array) -> np.array:
+    """Normalizes integer array [0,255] to [0,1]"""
+
+    pass
+
+
 def process_image(image_array: np.array) -> np.array:
     """Converts image to grayscale and returns normalized 0 to 1
 
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -4,29 +4,32 @@ TDD Testing functions corresponding to demo.py
 
 from unittest import TestCase
 import numpy as np
-from demo import process_image
+from demo import process_image, _convert_to_grayscale
 
 
 class TestProcessImage(TestCase):
-    def test_grayscale(self):
-        """Ensure input's third dimension becomes 1"""
+    def test_grayscale_dimensions(self):
+        """Ensure 3D output becomes 2D without changing other dimensions"""
 
         # Given
         input = [
-            np.random.randint(256, size=(1, 1, 3)),
-            np.random.randint(256, size=(10, 10, 3)),
-            np.random.randint(256, size=(100, 100, 3)),
+            np.random.randint(256, size=(3, 1, 1), dtype="B"),
+            np.random.randint(256, size=(3, 10, 10), dtype="B"),
+            np.random.randint(256, size=(3, 100, 100), dtype="B"),
         ]
 
         # Do
         for image in input:
-            actual_output = process_image(image)
+            actual_output = _convert_to_grayscale(image)
 
             # Compare
-            self.assertEqual(actual_output.ndim, 3)  # still want 3D input
+            self.assertEqual(actual_output.ndim, 2)  # Output should be 2D
             self.assertEqual(
-                actual_output.shape[2], 1
-            )  # but the 3rd dimension should be 1
+                image.shape[1:], actual_output.shape
+            )  # dimension and 1 and 2 should be unchanged
+
+    def test_grayscale_functionality(self):
+        self.fail
 
     def test_normalize(self):
         """Ensure values go from int [0, 255] to float [0, 1]"""
